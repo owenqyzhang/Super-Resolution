ace599b9189dc1acc3c214e86177e626ada315b2
Qiaoyu Zhang <owenqyzhang@gmail.com>
2017-12-13 19:23:28 -0500

    Fixed an error of loading global steps.

--- main.py
+++ main.py
@@ -5,18 +5,18 @@ import time
 Flags = tf.app.flags
 
 # System parameters
-Flags.DEFINE_string('output_dir', './experiment_SRResnet/', 'The output directory of the checkpoint.')
-Flags.DEFINE_string('summary_dir', './experiment_SRResnet/log/', 'The directory to store the summaries.')
+Flags.DEFINE_string('output_dir', './experiment_EDSR/', 'The output directory of the checkpoint.')
+Flags.DEFINE_string('summary_dir', './experiment_EDSR/log/', 'The directory to store the summaries.')
 Flags.DEFINE_string('mode', 'train', 'Mode for running: train, test, or inference.')
-Flags.DEFINE_string('checkpoint', None, 'If provided, the weights will be restored from the provided checkpoint.')
+Flags.DEFINE_string('checkpoint', './experiment_EDSR/model-520000', 'If provided, the weights will be restored from the provided checkpoint.')
 Flags.DEFINE_boolean('pre_trained_model', False, 'If set True, the weight will be loaded but the global steps will'
                                                  'still start from 0. If set False, global steps will be restored as'
                                                  'well.')
-Flags.DEFINE_string('pre_trained_model_type', 'SRResNet', 'The type of pretrained model (SRResNet, EDSR, ensemble,'
+Flags.DEFINE_string('pre_trained_model_type', 'EDSR', 'The type of pretrained model (SRResNet, EDSR, ensemble,'
                                                           'SRGAN, or EDSRGAN).')
 Flags.DEFINE_boolean('is_training', True, 'Training => True, Testing, Inference => False')
 Flags.DEFINE_string('vgg_ckpt', './vgg19/vgg_19.ckpt', 'Path to vgg19 checkpoint.')
-Flags.DEFINE_string('task', 'SRResNet', 'SRResNet, EDSR, ensemble, SRGAN or EDSRGAN')
+Flags.DEFINE_string('task', 'EDSR', 'SRResNet, EDSR, ensemble, SRGAN or EDSRGAN')
 
 # Data preprocessing configurations
 Flags.DEFINE_integer('batch_size', 16, 'Batch size of the input batch.')
@@ -33,7 +33,7 @@ Flags.DEFINE_integer('queue_thread', 32, 'The threads of the queue.')
 Flags.DEFINE_integer('num_resblocks', 16, 'Number of residual blocks in the generator.')
 
 # Content loss configurations
-Flags.DEFINE_string('perceptual_mode', 'VGG54', 'The type of feature used in perceptual loss.')
+Flags.DEFINE_string('perceptual_mode', 'MSE', 'The type of feature used in perceptual loss.')
 Flags.DEFINE_float('EPS', 1e-12, 'A small number added to prevent zero division.')
 Flags.DEFINE_float('ratio', 0.001, 'The ratio between content loss and adversarial loss.')
 Flags.DEFINE_float('vgg_scaling', 0.0061, 'The scaling factor of perceptual loss.')
@@ -300,7 +300,7 @@ elif FLAGS.mode == 'train':
 
         print('Optimization starts')
         start = time.time()
-        for step in range(max_iter):
+        for step in range(1, max_iter):
             fetches = {'train': net.train, 'global_step': sv.global_step}
 
             if ((step + 1) % FLAGS.display_freq) == 0:
@@ -321,6 +321,7 @@ elif FLAGS.mode == 'train':
                 fetches['summary'] = sv.summary_op
 
             results = sess.run(fetches)
+            step = results['global_step']
 
             if ((step + 1) % FLAGS.summary_freq) == 0:
                 print('Recording summary')
@@ -349,5 +350,8 @@ elif FLAGS.mode == 'train':
             if ((step + 1) % FLAGS.save_freq) == 0:
                 print('Save the checkpoint')
                 saver.save(sess, os.path.join(FLAGS.output_dir, 'model'), global_step=sv.global_step)
+
+            if step >= max_iter:
+                break
 
         print('Optimization done!')
